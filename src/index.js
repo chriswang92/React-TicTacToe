6aee474620520ecb1e4580694302c5f0c9f71f70
Chenguang Wang <wcg19920705@gmail.com>
2019-09-05 19:38:53 -0400

    0905-update3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Clock from './Clock.js';
+import Calculator from './TemperatureCalculator.js';
 
 // function components
 function GameDesc() {  
